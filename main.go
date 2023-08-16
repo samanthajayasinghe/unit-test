f1d63bf3af62e534842f8ef6ef31e72bf07e440f
Samantha Jayasinghe <sjayasin@redhat.com>
2023-08-16 16:35:08 +1200

    test

--- main.go
+++ main.go
@@ -12,13 +12,12 @@ var sender = slack.New(os.Getenv("SLACK_APP_TOKEN"))
 
 func main() {
 	a, b, c, err := sender.SendMessage(
-		"#pd-notifications",
-		slack.MsgOptionText("You have new notification, <https://example.com|ClusterOperatorDown> :email:", false),
+		"#pd-notification",
+		slack.MsgOptionText("You have new notification 2, <https://example.com|ClusterOperatorDown> :email:", false),
 	)
 	if err != nil {
 		logger.Errorln("Fail to send message", err)
 		return
 	}
 	logger.Infoln("Message Sent, result: ", a, b, c)
-
 }
